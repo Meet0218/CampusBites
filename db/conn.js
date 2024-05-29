1c18ed58f50ede8d24f70366224f6ed422af6e82
Meet <meetvadodariya2004@gmail.com>
2024-05-29 08:22:08 +0530

    Initial commit

--- db/conn.js
+++ db/conn.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables from .env file
 
+// Debugging log to check if the environment variable is loaded
+// console.log("Database URL from .env file: ", process.env.DATABASE);
+
+
 const db = process.env.DATABASE;
 
-mongoose  
+if (!db) {
+  console.error("Error: DATABASE environment variable is not defined.");
+  process.exit(1); // Exit the process with an error code
+}
+
+mongoose
   .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connection to Database successfully done"))
+  .then(() => console.log("Connection to Database successfully done!"))
   .catch((error) => console.log("Error: " + error.message));
