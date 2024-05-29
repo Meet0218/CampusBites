425614177eef037e876f04e3ced423c6c7a22a0b
Meet <meetvadodariya2004@gmail.com>
2024-05-29 10:04:18 +0530

    Initial commit

--- db/conn.js
+++ db/conn.js
@@ -4,8 +4,10 @@ require("dotenv").config(); // Load environment variables from .env file
 // Debugging log to check if the environment variable is loaded
 // console.log("Database URL from .env file: ", process.env.DATABASE);
 
+// const db = process.env.DATABASE;
 
-const db = process.env.DATABASE;
+const db =
+  "mongodb+srv://rajbodar720:raj123@cluster0.vysikqh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 if (!db) {
   console.error("Error: DATABASE environment variable is not defined.");
@@ -13,9 +15,6 @@ if (!db) {
 }
 
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(db)
   .then(() => console.log("Connection to Database successfully done!"))
   .catch((error) => console.log("Error: " + error.message));
